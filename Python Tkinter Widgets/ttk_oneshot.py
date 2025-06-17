fe72ae428dbe3aab9503c5041dd9340871483583
DIGITAL CREATIONS <85318556+DigitalCreationsLibrary@users.noreply.github.com>
2025-06-17 20:18:24 +0100

    Update ttk_oneshot.py

--- Python Tkinter Widgets/ttk_oneshot.py
+++ Python Tkinter Widgets/ttk_oneshot.py
@@ -16,55 +16,55 @@ print(s.theme_names())
 # Adding the common TTK Widgets
 
 ## The Tk Frames to Group all the widgets
-comon_widgets= tk.Frame(main_window,bg="#086788",borderwidth=2,relief="ridge")
-comon_widgets.pack(padx=5,pady=5,fill="both")
+common_widgets= tk.Frame(main_window,bg="#086788",borderwidth=2,relief="ridge")
+common_widgets.pack(padx=5,pady=5,fill="both")
 tk.Frame()
 
 
 ## The Actual Widgets
-button=ttk.Button(comon_widgets,text="TTK Button",)
+button=ttk.Button(common_widgets,text="TTK Button",)
 button.grid(padx=10,pady=5,row=0, column=0)
 
-check_button=ttk.Checkbutton(comon_widgets,text="Check Button",)
+check_button=ttk.Checkbutton(common_widgets,text="Check Button",)
 check_button.grid(padx=10,pady=5,row=0, column=1)
 
 val= tk.StringVar()
 val.set("TTK Entry")
-entry=ttk.Entry(comon_widgets,textvariable=val)
+entry=ttk.Entry(common_widgets,textvariable=val)
 entry.grid(padx=10,pady=5,row=0, column=2)
 
-frame=ttk.Frame(comon_widgets,width=150, height=60)
+frame=ttk.Frame(common_widgets,width=150, height=60)
 frame.grid(padx=10,pady=5,row=0, column=3,sticky="ew")
 # label_fframe = ttk.Label(frame, text="Label on Frame")
 # label_fframe.pack()
 
 
-label=ttk.Label(comon_widgets,text="TTK Label")
+label=ttk.Label(common_widgets,text="TTK Label")
 label.grid(padx=10,pady=5,row=1, column=0)
 
-label_frame=ttk.LabelFrame(comon_widgets,text="TTK Label Frame",width=150, height=60)
+label_frame=ttk.LabelFrame(common_widgets,text="TTK Label Frame",width=150, height=60)
 label_frame.grid(padx=10,pady=5,row=1, column=1)
 
-menu_button=ttk.Menubutton(comon_widgets,text="TTK Menubutton")
+menu_button=ttk.Menubutton(common_widgets,text="TTK Menubutton")
 menu_button.grid(padx=10,pady=5,row=1, column=2)
 menu_button.menu=tk.Menu(menu_button,tearoff=0)
 menu_button.menu.add("command",label="TTK Menu Item")
 menu_button["menu"]=menu_button.menu
 
-panel_window = ttk.PanedWindow(comon_widgets,orient="horizontal",width=150, height=60)
-panel_window.grid(padx=10,pady=5,row=1, column=3,sticky="ew")
-# label_fpanel = ttk.Label(panel_window, text="Label on Panel_Window")
-# label_fpanel.pack()
+paned_window = ttk.PanedWindow(common_widgets,orient="horizontal",width=150, height=60)
+paned_window.grid(padx=10,pady=5,row=1, column=3,sticky="ew")
+# label_fpaned = ttk.Label(paned_window, text="Label on Paned_Window")
+# label_fpaned.pack()
 
-radio_button=ttk.Radiobutton(comon_widgets,text="TTk Radio button",)
+radio_button=ttk.Radiobutton(common_widgets,text="TTk Radio button",)
 radio_button.grid(padx=10,pady=5,row=2, column=0)
 
 
-scale=ttk.Scale(comon_widgets,from_=0,to=100,orient="horizontal")
+scale=ttk.Scale(common_widgets,from_=0,to=100,orient="horizontal")
 scale.grid(padx=10,pady=5,row=2, column=1)
 
 
-scrollbar=ttk.Scrollbar(comon_widgets,orient="horizontal",)
+scrollbar=ttk.Scrollbar(common_widgets,orient="horizontal",)
 scrollbar.grid(padx=10,pady=5,row=2, column=2)
 
 
@@ -76,7 +76,7 @@ new_widgets=tk.Frame(main_window,bg="#086788",width=450,height=200,relief="ridge
 new_widgets.pack(padx=5,pady=5,fill="both")
 
 
-# sizegrip = ttk.Sizegrip(comon_widgets)
+# sizegrip = ttk.Sizegrip(common_widgets)
 # sizegrip.grid(padx=10,pady=5,row=3, column=0)
 ## The Actual Widgets
 
